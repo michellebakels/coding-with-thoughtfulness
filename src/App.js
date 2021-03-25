a4bc612ec70f52fa55f9124f07672555a349ec73
michellebakels <michelle.bakels@gmail.com>
2021-03-25 15:16:31 -0400

    implement recipe finder

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
+import {recipes} from "./recipes";
+
+const displayTime = (bakeTime) => {
+    const hours = (bakeTime / 60);
+    const rhours = Math.floor(hours);
+    const minutes = (hours - rhours) * 60;
+    const rminutes = Math.round(minutes);
+    return rhours < 1 ? `${rminutes} minutes` :
+        rhours === 1 ? `${rhours} hour and ${rminutes} minutes`
+            : `${rhours} hours and ${rminutes} minutes`
+}
 
 function App() {
+    const [bakeTime, setBakeTime] = useState("1000")
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page-container" className="page-container">
+      <h1>That's So Sweet!</h1>
+      <h2>Let someone know you're thinking of them by sharing something sweet</h2>
+        <h3>Find a recipe that works with your schedule</h3>
+        <div id="recipe-selector" className="recipe-selector">
+            <label>Total Bake Time:&nbsp;
+                <select name="bakeTimeSelector" id="bake-time-selector" value={bakeTime} onChange={(e) => setBakeTime(e.target.value)}>
+                    <option value="15">15 minutes or less</option>
+                    <option value="30">30 minutes or less</option>
+                    <option value="60">1 hour or less</option>
+                    <option value="1000">Time is a construct - Show All</option>
+                </select>
+            </label>
+        </div>
+        <div className="recipe-cards">
+          {
+            recipes.map(recipe => {
+                return (
+                (recipe.time <= bakeTime) &&
+                    <a href={recipe.link} target="_blank" style={{textDecoration: 'none'}}>
+                        <div className="card">
+                            <div id="bake-time" className="bake-time">{displayTime(recipe.time)}</div><br/>
+                            <div id="image-item" className="item">
+                                <img src={recipe.image} alt={recipe.alt} className="card-img"/>
+                            </div>
+                            <h4>{recipe.title}</h4><br/>
+                            <br/>
+                            <br/>
+                        </div>
+                    </a>
+                )
+            })
+          }
+        </div>
+      <footer>All recipes provided by the culinary visionary, Tieghan Gerard, of <a href="https://www.halfbakedharvest.com/" target="_blank">Half Baked Harvest</a></footer>
     </div>
   );
 }
---
+++ src/recipes.js
@@ -0,0 +1,100 @@
+export const recipes = [
+    {
+        title: 'vintage chocolate peanut butter pie',
+        time: 105,
+        link: 'https://www.halfbakedharvest.com/chocolate-peanut-butter-pie/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2021/03/Vintage-Chocolate-Peanut-Butter-Pie-11.jpg',
+        alt: 'Slice of chocolate peanut butter pie with chocolate shavings sprinkled around'
+    },
+    {
+        title: 'soft and crinkly brown sugar peanut butter cookies',
+        time: 25,
+        link: 'https://www.halfbakedharvest.com/brown-sugar-peanut-butter-cookies/',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2021/03/Soft-and-Crinkly-Brown-Sugar-Peanut-Butter-Cookies-8.jpg',
+        alt: 'Brown sugar peanut butter cookies with a chocolate sauce drizzle'
+    },
+    {
+        title: 'salted tahini butter chocolate chip cookies',
+        time: 20,
+        link: 'https://www.halfbakedharvest.com/tahini-butter-chocolate-chip-cookies/',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2021/01/Salted-Tahini-Butter-Chocolate-Chip-Cookies-7.jpg',
+        alt: 'Chocolate chip cookies, half flipped over to show golden brown underneath'
+    },
+    {
+        title: 'brown butter malted chocolate chunk cookies',
+        time: 40,
+        link: 'https://www.halfbakedharvest.com/brown-butter-malted-chocolate-chunk-cookies/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2021/02/Brown-Butter-Malted-Chocolate-Chunk-Cookies.-6.jpg',
+        alt: 'Chocolate chunk cookies with pink rose decoration'
+    },
+    {
+        title: 'malted milk cookies n\' cream ice cream sandwiches.',
+        time: 315,
+        link: 'https://www.halfbakedharvest.com/malted-milk-cookies-n-cream-ice-cream-sandwiches/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2020/07/Malted-Milk-Cookies-n%E2%80%99-Cream-Ice-Cream-Sandwiches-10.jpg',
+        alt: 'Stack of cookies and cream ice cream sandwiches'
+    },
+    {
+        title: 'salted caramel pretzel snickerdoodle',
+        time: 40,
+        link: 'https://www.halfbakedharvest.com/salted-caramel-pretzel-snickerdoodles/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2019/12/Salted-Caramel-Pretzel-Snickerdoodles-8.jpg',
+        alt: 'Snickerdoodles with visible chunks of chocolate and pretzel baked in'
+    },
+    {
+        title: 'salted chocolate covered pretzel + nutella turtle cookies',
+        time: 25,
+        link: 'https://www.halfbakedharvest.com/idiot-proof-salted-chocolate-covered-pretzel-nutella-turtle-cookies/',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2013/11/Idiot-Proof-Salted-Chocolate-Covered-Pretzel-+-Nutella-Turtle-Cookies-10.jpg',
+        alt: 'turtle cookies covered with chocolate and course sea salt'
+    },
+    {
+        title: '5-ingredient puppy chow ritz cracker cookies',
+        time: 22,
+        link: 'https://www.halfbakedharvest.com/idiot-proof-5-ingredient-puppy-chow-ritz-cracker-cookies/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2014/11/Idiot-Proof-5-Ingredient-Puppy-Chow-Ritz-Cracker-Cookies-101.jpg',
+        alt: 'puppy chow cookies split in half to show the layers covered with powdered sugar'
+    },
+    {
+        title: 'loaded chocolate hazelnut liquor turnovers with salted vanilla bean whipped cream',
+        time: 95,
+        link: 'https://www.halfbakedharvest.com/loaded-chocolate-hazelnut-liquor-turnovers-salted-vanilla-bean-whipped-cream/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2014/11/Loaded-Chocolate-Hazelnut-Liquor-Turnovers-with-Salted-Vanilla-Bean-Whipped-Cream-81.jpg',
+        alt: 'chocolate hazelnut liquor turnover triangles with small dish of vanilla bean whipped cream and serving spoon'
+    },
+    {
+        title: 'single girls wholesome molten chocolate cake',
+        time: 15,
+        link: 'https://www.halfbakedharvest.com/single-girls-wholesome-molten-chocolate-lava-cake/#bo-recipee',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2017/02/Single-Girls-Wholesome-Molten-Chocolate-Lava-Cake-7.jpg',
+        alt: 'small molten chocolate cake with dollop of cream on top garnished with flowers around'
+    },
+    {
+        title: '5 minute molten chocolate mug cake',
+        time: 5,
+        link: 'https://www.halfbakedharvest.com/single-lady-5-minute-gooey-molten-chocolate-mug-cake/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2020/04/5-Minute-Molten-Chocolate-Mug-Cake-7.jpg',
+        alt: 'coffee mug with filled with chocolate cake and a scoop of vanilla ice cream on top'
+    },
+    {
+        title: 'flourless chocolate espresso olive oil cake with honey whipped ricotta',
+        time: 45,
+        link: 'https://www.halfbakedharvest.com/flourless-chocolate-espresso-cake/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2020/02/Flourless-Chocolate-Espresso-Cake-7.jpg',
+        alt: 'slice of chocolate espresso cake with honey whipped ricotta on top being cut by a fork'
+    },
+    {
+        title: 'easiest coconut banana cake with fudgy chocolate frosting',
+        time: 50,
+        link: 'https://www.halfbakedharvest.com/coconut-banana-cake/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2020/02/Easiest-Coconut-Banana-Cake-with-Fudgy-Chocolate-Frosting-9.jpg',
+        alt: 'coconut banana cake cut in squares with half showing fudge top and half turned on the side to show the cake and frosting layers'
+    },
+    {
+        title: 'matcha green tea cake with vanilla honey cream cheese frosting',
+        time: 55,
+        link: 'https://www.halfbakedharvest.com/matcha-green-tea-cake-with-vanilla-honey-cream-cheese-frosting/#bo-recipe',
+        image: 'https://www.halfbakedharvest.com/wp-content/uploads/2015/04/Matcha-Green-Tea-Cake-with-Vanilla-Honey-Cream-Cheese-Frosting-4.jpg',
+        alt: 'cake stand with matcha green tea cake flanked by two vases of spring flowers'
+    }
+]
